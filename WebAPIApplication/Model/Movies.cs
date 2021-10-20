2804dafb5dffc2f760fc388ea2e98bc3b970070f
augustoje <augusto.jesus@outlook.com.br>
2021-10-20 00:20:02 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPIApplication/Model/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIApplication.Model
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public DateTime ReleaseData { get; set; }
+        public string genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
